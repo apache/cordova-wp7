755025a40b08fae726af4ab6031fa98d3e625b23
Jesse MacFadyen <purplecabbage@gmail.com>
2012-12-03 16:32:26 -0800

    added using, instead of fully qualified namespace

--- templates/standalone/cordovalib/CordovaView.xaml.cs
+++ templates/standalone/cordovalib/CordovaView.xaml.cs
@@ -37,6 +37,7 @@ using System.Text;
 using WP7CordovaClassLib.Cordova;
 using System.Threading;
 using Microsoft.Phone.Shell;
+using WP7CordovaClassLib.Cordova.JSON;
 
 
 
@@ -421,7 +422,7 @@ namespace WP7CordovaClassLib
                 switch (commandCallParams.Action.ToLower())
                 {
                     case "overridebackbutton":
-                        string arg0 = WP7CordovaClassLib.Cordova.JSON.JsonHelper.Deserialize<string[]>(commandCallParams.Args)[0];
+                        string arg0 = JsonHelper.Deserialize<string[]>(commandCallParams.Args)[0];
                         this.OverrideBackButton = (arg0 != null && arg0.Length > 0 && arg0.ToLower() == "true"); 
                         break;
                 }
